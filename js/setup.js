174d287bf818f3942eda8f918ccd6534edc271bc
Kirill Yun <twistedfact@gmail.com>
2020-02-13 19:42:17 +0300

    Сделан блок с похожими волшебниками

---
+++ js/setup.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var numberOfWizards = 4;
+
+var getWizards = function () {
+  var wizard = [];
+  for (var i = 0; i < numberOfWizards; i++) {
+    wizard.push({
+      name: WIZARD_NAMES[getRandomInt(0, WIZARD_NAMES.length)] + ' ' + WIZARD_SECOND_NAMES[getRandomInt(0, WIZARD_SECOND_NAMES.length)],
+      coatColor: COAT_COLORS[getRandomInt(0, COAT_COLORS.length)],
+      eyesColor: EYES_COLORS[getRandomInt(0, EYES_COLORS.length)]
+    });
+  }
+  return wizard;
+};
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+var wizards = getWizards();
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+var similarWizardsBlock = document.querySelector('.setup-similar');
+similarWizardsBlock.classList.remove('hidden');
